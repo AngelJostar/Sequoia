5e980aeba9516fc089b74f0192b136b54fb1358c
AngelJostar <angelrojas@ciencias.unam.mx>
2021-01-10 19:23:54 -0600

    commit 5

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -3,5 +3,8 @@
 <div class="texto-footer">
     <p>Para aclarar dudas comunícate con nuestros especialistas:</p>
 </div>
+<div class="centrar-texto">
+    <a href="../contacto "><button class="boton boton-footer">Contacto</button></a>
+</div>
 
 </footer>
