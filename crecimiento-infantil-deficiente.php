b3a6dc89227fa6ad179a13bce3ae1c3d4ad053a7
AngelJostar <angelrojas@ciencias.unam.mx>
2021-01-12 21:12:43 -0600

    commit font-awesome

---
+++ crecimiento-infantil-deficiente.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once 'includes/tags/google_tag_manager_head.php';?>
+
+<link rel="icon" type="image/png" href="img/cropped-favicon-1-32x32.png" sizes="32x32">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<title>Crecimiento - infantil deficiente</title>
+<link rel="stylesheet" rel="preload" href="css/style.css" media="all">
+<link rel="stylesheet" rel="preload" href="css/normalize.css" media="all">
+
+</head>
+
+<?php include_once 'includes/tags/google_tag_manager_body.php';?>
+<div id="top-header" class="top-header">
+    <div class="top-header-inter">
+        <a href="tel:+5555238959">
+            <span>
+                        &nbsp; Tel. CDMX: <br> 
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (55)5687-6118
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  &nbsp; Tel. GDL: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (33)3615-2835
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  
+                        &nbsp; Tel. MTY: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                         (81)11592265
+                        </span>
+            </span>
+        </a>
+    </div>
+</div>
+<!--####HEADER####-->
+<section id="site-wrapper">
+
+    <!-- CANVAS -->
+    <section id="site-canvas">
+
+        <?php include_once 'includes/templates/header.php';?>
+
+
+        <div class="contendor-crecimiento">
+            <div class="cuadros-crecimiento">
+                <h1>Aprende a reconocer el crecimiento deficiente de tu hijo
+                </h1>
+                <p>Es muy importante que sepas si la tasa de crecimiento del niño es la que debería ser, a cualquier edad. Si el niño no está creciendo adecuadamente, puede ser el primer signo de una posible condición médica. Esta página puede orientarte
+                    sobre los signos claves que indicarían un crecimiento deficiente y lo que pueden significar para la salud del niño.</p>
+            </div>
+            <div class="contenedor-infantil-deficiente">
+                <div class="justificar-texto-90">
+                    <p>Cualquiera que sea la edad de su hijo, es importante saber si está creciendo como debería. Si la tasa de crecimiento de su hijo no se encuentra dentro del rango esperado, podría ser un signo de una condición de crecimiento inadecuado.
+                    </p>
+                </div>
+                <div class="contenedor-video">
+                    <div class="reproductor" data-id="BXOOo5c3LGM"></div>
+                </div>
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>¿Cómo reconocer un crecimiento deficiente en tu hijo?</h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>Hay señales a las que debe prestar atención que podrían indicar que su hijo no está creciendo como debería: </p>
+                </div>
+                <div>
+                    
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <?php include_once 'includes/templates/footer.php';?>
+
+    </section>
+    <!-- END SITE CONTENT -->
+
+</section>
+<!--####HEADER####-->
+
+<a href="#" class="scrollup"><i class="fas fa-angle-double-up fa-2x"></i></a>
+<!--####FOOTER####-->
+<script src="https://kit.fontawesome.com/3919625d45.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/scripts.js "></script>
+</body>
+
+
+</html>
--- crecimiento-infantil.php
+++ crecimiento-infantil.php
@@ -7,7 +7,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="format-detection" content="telephone=no">
-<title>Contact - Centro Sequoia</title>
+<title>Crecimiento - infantil</title>
 <link rel="stylesheet" rel="preload" href="css/style.css" media="all">
 <link rel="stylesheet" rel="preload" href="css/normalize.css" media="all">
 
@@ -51,29 +51,77 @@
     <section id="site-canvas">
 
         <?php include_once 'includes/templates/header.php';?>
-
-
+        <i class="fas fa-abacus"></i>
         <div class="contendor-crecimiento">
             <div class="cuadros-crecimiento">
-                    <h1>Todo lo que debes saber sobre crecimiento infantil</h1>
-                    <p>En Centro Sequoia queremos que tomes decisiones informadas, por eso te presentamos a continuación toda la información que necesitas para entender el crecimiento de tu hijo o hija.</p>
+                <h1>Todo lo que debes saber sobre crecimiento infantil</h1>
+                <p>En Centro Sequoia queremos que tomes decisiones informadas, por eso te presentamos a continuación toda la información que necesitas para entender el crecimiento de tu hijo o hija.</p>
             </div>
             <div class="contenedor-video">
                 <div class="reproductor" data-id="BXOOo5c3LGM"></div>
             </div>
             <div class="justificar-texto-90">
-                <p>A continuación puedes explorar los temas más
-                    importantes relacionados con el crecimiento de tu
-                    niño o niña:
+                <p>A continuación puedes explorar los temas más importantes relacionados con el crecimiento de tu niño o niña:
                 </p>
             </div>
             <div class="curvi-cuadros-crecimiento ">
                 <h2>CRECIMIENTO INFANTIL GENERALIDADES</h2>
-                <p>Te presentamos toda la información necesaria para
-                entender a fondo el crecimiento de tu hijo o hija</p>
+                <p>Te presentamos toda la información necesaria para entender a fondo el crecimiento de tu hijo o hija</p>
+                <div class="aprende-mas">
+                    <p>APRENDE MÁS</p>
+                </div>
             </div>
-            <div class="">
-                <p>APRENDE MÁS</p>
+
+            <div class="curvi-cuadros-crecimiento ">
+                <h2>CRECIMIENTO INFANTIL GENERALIDADES</h2>
+                <p>Te presentamos toda la información necesaria para entender a fondo el crecimiento de tu hijo o hija</p>
+                <div class="aprende-mas">
+                    <a href="">
+                        <p>APRENDE MÁS</p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="curvi-cuadros-crecimiento ">
+                <h2>CRECIMIENTO INFANTIL POR EDAD DE NIÑOS Y ADOLECENTES</h2>
+                <p>El crecimiento de niños y niñas puede variar según su edad. Las tasas de crecimiento dependen de la edad de los niños y niñas.</p>
+                <div class="aprende-mas">
+                    <a href="">
+                        <p>APRENDE MÁS</p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="curvi-cuadros-crecimiento ">
+                <h2>CAUSAS DE LA BAJA ESTATURA</h2>
+                <p>Los diferentes trastornos del crecimiento pueden afectar la salud de un niño de muchas formas diferentes y pueden ser causados por una variedad de factores.</p>
+                <div class="aprende-mas">
+                    <a href="">
+                        <p>APRENDE MÁS</p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="curvi-cuadros-crecimiento ">
+                <h2>¿CÓMO SABER SI TU HIJO O HIJA ESTA CRECIENDO BIEN?</h2>
+                <p>Aprende a reconocer el crecimiento deficiente en tu hijo o hija. Si la tasa de crecimiento de su hijo no se encuentra dentro del rango esperado, podría ser un signo de una condición de crecimiento inadecuado.
+                </p>
+                <div class="aprende-mas">
+                    <a href="">
+                        <p>APRENDE MÁS</p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="curvi-cuadros-crecimiento ">
+                <h2>IMPORTANCIA DE SER MAS ALTO
+                </h2>
+                <p>Además de tener más confianza y habilidades sociales debidas a su complexión física, las personas altas también pueden presumir de mayor sueldo, más likes en redes sociales y también de una productividad laboral más amplia.</p>
+                <div class="aprende-mas">
+                    <a href="">
+                        <p>APRENDE MÁS</p>
+                    </a>
+                </div>
             </div>
         </div>
 
@@ -93,7 +141,7 @@
 
 <a href="#" class="scrollup"><i class="fas fa-angle-double-up fa-2x"></i></a>
 <!--####FOOTER####-->
-<script src="https://kit.fontawesome.com/3919625d45.js" crossorigin="anonymous"></script>
+<script src="1351.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <script src="js/jquery-3.5.1.min.js"></script>
