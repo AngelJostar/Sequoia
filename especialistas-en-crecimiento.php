ab884f76563947ae9dc6d4a1ad18dd7b7d7c3058
ItzelDelgado <itzeldelgado314@ciencias.unam.mx>
2021-01-12 23:46:07 -0600

    se-agrego-clase-especialistas-en-crecimiento

---
+++ especialistas-en-crecimiento.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once 'includes/tags/google_tag_manager_head.php';?>
+
+<link rel="icon" type="image/png" href="img/cropped-favicon-1-32x32.png" sizes="32x32">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<title>Crecimiento - especialistas-en-crecimiento</title>
+<link rel="stylesheet" rel="preload" href="css/style.css" media="all">
+<link rel="stylesheet" rel="preload" href="css/normalize.css" media="all">
+
+</head>
+
+<?php include_once 'includes/tags/google_tag_manager_body.php';?>
+<div id="top-header" class="top-header">
+    <div class="top-header-inter">
+        <a href="tel:+5555238959">
+            <span>
+                        &nbsp; Tel. CDMX: <br> 
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (55)5687-6118
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  &nbsp; Tel. GDL: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (33)3615-2835
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  
+                        &nbsp; Tel. MTY: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                         (81)11592265
+                        </span>
+            </span>
+        </a>
+    </div>
+</div>
+<!--####HEADER####-->
+<section id="site-wrapper">
+
+    <!-- CANVAS -->
+    <section id="site-canvas">
+
+        <?php include_once 'includes/templates/header.php';?>
+
+
+        <div class="contendor-crecimiento">
+            <div class="cuadros-crecimiento">
+                <h1>El especialista adecuado para
+                    tratar el crecimiento infantil es el
+                    endocrinólogo pediatra
+                    especializado en crecimiento.
+                </h1>
+                <p>Es muy importante que sepas que existen
+                    médicos, pediatras con subespecialidad en
+                    endocrinología pediátrica, que además después
+                    se enfocan en tratar únicamente la estatura de
+                    los niños y niñas.</p>
+            </div>
+            <div class="contenedor-infantil-deficiente">
+                <div class="justificar-texto-90">
+                    <p>Si su hijo o hija tiene problemas de crecimiento,
+                        pubertad u otros trastornos relacionados con
+                        las hormonas y las glándulas que las producen,
+                        un endocrinólogo es el especialista adecuado.
+                    </p>
+                </div>
+                <div class="contenedor-video">
+                    <div class="reproductor" data-id="BXOOo5c3LGM"></div>
+                </div>
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>El Pediatra Endocrinólogo es el
+                        especialista encargado de los problemas
+                        del crecimiento.</h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>Las hormonas son sustancias químicas que afectan
+                        el funcionamiento muchas partes del cuerpo. Por
+                        ejemplo, las hormonas deciden cómo crece y
+                        madura un niño. La endocrinología pediátrica es la
+                        ciencia que estudia estas glándulas y los efectos de
+                        las hormonas en los niños.
+                        </p>
+                </div>
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>¿Cuándo acudir con un especialista en
+                    crecimiento infantil?</h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>La baja estatura con un crecimiento anormalmente
+                    lento es la preocupación más común a la que hay
+                    que estar pendientes . Un niño más bajo que sus
+                    compañeros es el claro ejemplo de que hay que
+                    visitar al especialista en crecimiento.
+                    </p>
+                </div>
+                <div class="especialistas-en-crecimiento-negritas">
+                    <p>Agenda una cita con nuestros especialistas cuando
+                    tu hijo o hija:
+                    </p>
+                </div>
+                <div class="grid-iconos">
+                    <div>
+                        <i class="fad fa-tshirt"></i>
+                        <p class="padding-2">Gasta la ropa antes que se le quede chiquita</p>
+                        <i class="fas fa-child"></i>
+                        <p>Lo confunden con alguien más joven</p>
+                    </div>
+                    <div>
+                        <i class="fas fa-tape"></i>
+                        <p>Mucho más bajo que los compañeros de clase u otros de su edad</p>
+                        <i class="far fa-thumbs-down"></i>
+                        <p>Otros niños se burlan de su estatura</p>
+                    </div>
+                    <div>
+                        <i class="fas fa-user-friends"></i>
+                        <p>Más bajo que tú/tus otros hijos a las misma edad</p>
+                    </div>
+
+                </div>
+
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>¿Por qué acudir a Centro Sequoia?</h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>En Centro Sequoia única y exclusivamente atendemos los
+                    padecimientos que afectan la estatura de los niños. Nuestros
+                    médicos especialistas tienen la experiencia de muchos miles
+                    de casos atendidos en este campo, lo que nos lleva a ser el
+                    centro más especializado en crecimiento de México.
+                    </p>
+                </div>
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>Los endocrinólogos pediatras especializados
+                    en crecimiento infantil de Centro Sequoia
+                    son la mejor opción para los niños y niñas
+                    que busquen conseguir una mayor estatura. </h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>Los problemas hormonales que afectan el crecimiento o
+                    el desarrollo sexual de un adolecente o un niño pueden
+                    tener efectos significativos en el bienestar físico y
+                    emocional del mismo. En Centro Sequoia somos sensibles
+                    a estos temas y entendemos a su hijo. El personal de
+                    apoyo, incluidos enfermeras, psicólogos y nutriólogos
+                    estamos en sintonía con las necesidades de los niños y
+                    adolescentes que atendemos. Con el objetivo de lograr
+                    que todos nuestros pacientes consigan una mayor
+                    estatura.
+                    </p>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <?php include_once 'includes/templates/footer.php';?>
+
+    </section>
+    <!-- END SITE CONTENT -->
+
+</section>
+<!--####HEADER####-->
+
+<a href="#" class="scrollup"><i class="fas fa-angle-double-up fa-2x"></i></a>
+<!--####FOOTER####-->
+<script src="1351.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/scripts.js "></script>
+</body>
+
+
+</html>
