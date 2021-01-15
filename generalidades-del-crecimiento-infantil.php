7587d1930c3cbcf2210c55b9fb78fc8d6077f39d
ItzelDelgado <itzeldelgado314@ciencias.unam.mx>
2021-01-15 11:31:05 -0600

    se-agrego-pestaña-generalidades-de-crecimiento

---
+++ generalidades-del-crecimiento-infantil.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once 'includes/tags/google_tag_manager_head.php';?>
+
+<link rel="icon" type="image/png" href="img/cropped-favicon-1-32x32.png" sizes="32x32">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<title>Crecimiento - generalidades-del-crecimiento-infantil</title>
+<link rel="stylesheet" rel="preload" href="css/style.css" media="all">
+<link rel="stylesheet" rel="preload" href="css/normalize.css" media="all">
+
+</head>
+
+<?php include_once 'includes/tags/google_tag_manager_body.php';?>
+<div id="top-header" class="top-header">
+    <div class="top-header-inter">
+        <a href="tel:+5555238959">
+            <span>
+                        &nbsp; Tel. CDMX: <br> 
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (55)5687-6118
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  &nbsp; Tel. GDL: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (33)3615-2835
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  
+                        &nbsp; Tel. MTY: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                         (81)11592265
+                        </span>
+            </span>
+        </a>
+    </div>
+</div>
+<!--####HEADER####-->
+<section id="site-wrapper">
+
+    <!-- CANVAS -->
+    <section id="site-canvas">
+
+        <?php include_once 'includes/templates/header.php';?>
+
+
+        <div class="contendor-crecimiento">
+            <div class="cuadros-crecimiento">
+                <h1>Crecimiento infantil-generalidades
+
+                </h1>
+                <p>El crecimiento es el proceso biológico más
+                    característico de la infancia. Se inicia en el
+                    momento de la fecundación del óvulo y se
+                    extiende hasta que finaliza la maduración de los
+                    huesos y se completa el desarrollo sexual, lo que
+                    ocurre habitualmente, hacia el final de la
+                    segunda década de la vida</p>
+            </div>
+            <div class="contenedor-generalidades">
+                <div class="contenedor-video">
+                    <div class="reproductor" data-id="BXOOo5c3LGM"></div>
+                </div>
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>Temas importantes relacionados con el
+                        crecimiento de tu hijo o hija</h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>La desinformación puede retrasar las intervenciones
+                    cuando realmente es necesario hacer algo. A
+                    continuación puedes explorar los temas más
+                    importantes relacionados con el crecimiento de tu
+                    niño o niña:</p>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Factores que afectan el crecimiento</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">¿Cómo debes medir a tu hijo?</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Baja estatura</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Graficas de Crecimiento</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">La nutrición y el crecimiento</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Radiografía de Edad Ósea*</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Pubertad precoz o atrasada</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Talla genética o blanco familiar</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Cambios físicos en niños y niñas</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Estirón Puberal</button></a>
+                </div>
+                <p>Video</p>
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>Crecimiento Infantil</h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>El crecimiento infantil es un proceso
+                    extremadamente complejo en el que intervienen
+                    diversos factores y es uno de los indicadores del
+                    estado de salud del niño, ya que muchas
+                    enfermedades pueden dar como primera
+                    manifestación la detención del crecimiento.
+                    </p>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>Para la adecuada valoración de la talla de un niño
+                        es importante considerar los distintos factores que
+                        intervienen
+                        e interactúan en el correcto desarrollo
+                        infantil:
+
+                    </p>
+                </div>
+
+                <div class="contenedor-generalidades">
+                    <div class="generalidades-negritas">
+                        <p>Factores endógenos(propios del niño):
+                        </p>
+                    </div>
+                    <div class="justificar-texto-90-listas">
+                        <ul type="square">
+                            <li> Factores genéticos del crecimiento: Existen
+                                genes en el cuerpo humano que participan en el
+                                crecimiento de un niño
+                                y que son heredados de
+                                ambos padres
+                                . Los genes del crecimiento de los
+                                papás determinan el 80
+                                % de la talla final adulta
+                                de su hijo.</li>
+                            <li>Factores hormonales del crecimiento: Durante
+                                la etapa fetal interactúan genes
+                                y hormonas
+                                producidas por el bebe
+                                y la placenta para
+                                cumplir la función del crecimiento
+                                y después del
+                                nacimiento la hormona de crecimiento
+                                y las
+                                hormonas tiroideas realizarán esta función. En
+                                la pubertad las hormonas sexuales (que son las
+                                que dan los cambios de la adolescencia) se
+                                suman a la función del crecimiento para
+                                culminar en la talla final adulta.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="contenedor-generalidades">
+                    <div class="generalidades-negritas">
+                        <p>Factores Exógenos(externos):
+                        </p>
+                    </div>
+                    <div class="justificar-texto-90-listas">
+                        <ul type="square">
+                            <li> La alimentación correcta del niño aporta el
+                                material energético necesario para su
+                                crecimiento. La desnutrición o enfermedades
+                                que no permitan la absorción adecuada de los
+                                nutrientes pueden dar como resultado una talla
+                                baja.</li>
+                            <li>Hábitos: La práctica regular del ejercicio para
+                                niños es beneficioso para el crecimiento, no así el
+                                de competición o alto rendimiento. El sueño
+                                durante la noche y de manera temprana es otro
+                                factor que influye en la producción de la
+                                hormona de crecimiento natural.</li>
+                            <li>Psicológicos y afectivos. Alguna privación
+                                emocional, anorexia y depresión pueden
+                                provocar alteraciones en el crecimiento.</li>
+                            <li>De este modo, la talla máxima adulta se verá
+                                influenciada por la interacción de estos factores.
+                                La expresión de los genes dependerá del estado
+                                de salud, psicológico y nutricional del niño.</li>
+                            <li>Actualmente el tema de crecimiento infantil es
+                                importante ya que la talla baja no solo afecta de
+                                manera psicológica (autoestima, bullying, etc),
+                                sino también en el desarrollo social e incluso en
+                                la vida laboral.
+                                </li>
+                        </ul>
+                    </div>
+                </div>
+                <p>Banner grande vertical A contacto</p>
+            
+
+            </div>
+
+
+        </div>
+
+        <?php include_once 'includes/templates/footer.php';?>
+
+    </section>
+    <!-- END SITE CONTENT -->
+
+</section>
+<!--####HEADER####-->
+
+<a href="#" class="scrollup"><i class="fas fa-angle-double-up fa-2x"></i></a>
+<!--####FOOTER####-->
+<script src="1351.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/scripts.js "></script>
+</body>
+
+
+</html>
