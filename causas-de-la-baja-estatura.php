35985c573241cb818c4af3b4b975a689ac2bad18
ItzelDelgado <itzeldelgado314@ciencias.unam.mx>
2021-01-15 23:43:07 -0600

    se-agrego-causas-de-la-baja-estatura

---
+++ causas-de-la-baja-estatura.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once 'includes/tags/google_tag_manager_head.php';?>
+
+<link rel="icon" type="image/png" href="img/cropped-favicon-1-32x32.png" sizes="32x32">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<title>Crecimiento - causas-de-la-baja-estatura</title>
+<link rel="stylesheet" rel="preload" href="css/style.css" media="all">
+<link rel="stylesheet" rel="preload" href="css/normalize.css" media="all">
+
+</head>
+
+<?php include_once 'includes/tags/google_tag_manager_body.php';?>
+<div id="top-header" class="top-header">
+    <div class="top-header-inter">
+        <a href="tel:+5555238959">
+            <span>
+                        &nbsp; Tel. CDMX: <br> 
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (55)5687-6118
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  &nbsp; Tel. GDL: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                        (33)3615-2835
+                        </span>
+            </span>
+        </a>
+        <a href="tel:+5555238959">
+            <span>  
+                        &nbsp; Tel. MTY: <br>
+                        <span>
+                        <i class="fas fa-phone"></i>
+                         (81)11592265
+                        </span>
+            </span>
+        </a>
+    </div>
+</div>
+<!--####HEADER####-->
+<section id="site-wrapper">
+
+    <!-- CANVAS -->
+    <section id="site-canvas">
+
+        <?php include_once 'includes/templates/header.php';?>
+
+
+        <div class="contendor-crecimiento">
+            <div class="cuadros-crecimiento">
+                <h1>Causas de la baja estatura</h1>
+                <p>Un crecimiento más lento de lo normal puede
+                    ser señal de un problema de salud importante.
+                    Así mismo algunos de los niños que tienen baja
+                    estatura que no está relacionada con alguna
+                    afección médica también pueden querer
+                    alcanzar una mayor estatura para sobresalir
+                    física y socialmente.</p>
+            </div>
+            <div class="contenedor-generalidades">
+                <div class="contenedor-video">
+                    <div class="reproductor" data-id="BXOOo5c3LGM"></div>
+                </div>
+                <div class="contenedor-generalidades">
+                    <div class="generalidades-negritas">
+                        <p>Entre las causas de una baja estatura que NO SE ASOCIAN a enfermedades reconocidas, se
+                        incluyen:
+                        </p>
+                    </div>
+                    <div class="justificar-texto-90-listas">
+                        <ul>
+                            <li>Baja estatura en la familia</li>
+                            <li>Baja estatura idiopática (sin causa identificable).</li>
+                            <li>Retraso general del crecimiento y la pubertad.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="contenedor-generalidades">
+                    <div class="generalidades-negritas">
+                        <p>Entre las afecciones médicas que SI SE ASOCIAN
+                        con el crecimiento, se incluyen:
+
+                        </p>
+                    </div>
+                    <div class="justificar-texto-90-listas">
+                        <ul>
+                            <li>Mala nutrición</li>
+                            <li>Niños pequeños al nacer.</li>
+                            <li>Deficiencias hormonales.</li>
+                            <li>Padecimientos y afecciones médicas crónicas.</li>
+                            <li>Afecciones genéticas.</li>
+                            <li>Medicamentos.</li>
+                        </ul>
+                    </div>
+                </div>
+                <p>X Banner Chico Importancia de ser mas alto</p>
+                <div class="encabezado-crecimiento-deficiente">
+                    <h2>Obtenga más información sobre las causas específicas del crecimiento deficiente</h2>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>Los diferentes trastornos del crecimiento pueden afectar la
+                        salud de un niño de muchas formas diferentes y pueden ser
+                        causados por una variedad de factores. Explora los enlaces a
+                        continuación para aprender a fondo todo acerca de las causas
+                        mas comunes de una baja estatura en tu niño o niña. </p>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Baja estatura familiar</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Baja estatura idiopática</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Retraso del crecimiento y pubertad</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Malnutrición y malabsorción </button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Niños pequeños al nacer</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Deficiencia de hormona de
+                    crecimiento</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Padecimientos y Enfermedades
+                    crónicas</button></a>
+                </div>
+                <div class="centrar-texto texto-boton-esp pb-2">
+                    <a href="../contacto "><button class="boton-e boton-esp">Síndromes que afectan el
+                    crecimiento</button></a>
+                </div>
+                <div class="justificar-texto-90">
+                    <p>Te invitamos a que agendes una cita para que uno
+                        de nuestros médicos endocrinólogos pediatras
+                        especialistas en crecimiento pueda hacer un
+                        diagnóstico personalizado de tu hijo y diseñar el
+                        tratamiento adecuado en caso de requerirlo.
+                    </p>
+                </div>     
+                <p>Banner
+                    Una consulta con un
+                    especialista no es motivo para
+                    alarma, pero ya que tu niño tiene
+                    un tiempo limitado para crecer,
+                    debes darle seguimiento lo
+                    antes posible
+                    CONTACTANOS</p>
+            
+
+            </div>
+
+
+        </div>
+
+        <?php include_once 'includes/templates/footer.php';?>
+
+    </section>
+    <!-- END SITE CONTENT -->
+
+</section>
+<!--####HEADER####-->
+
+<a href="#" class="scrollup"><i class="fas fa-angle-double-up fa-2x"></i></a>
+<!--####FOOTER####-->
+<script src="1351.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/scripts.js "></script>
+</body>
+
+
+</html>
