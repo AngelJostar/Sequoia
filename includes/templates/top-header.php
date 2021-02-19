8beec0a712222c983bf1b03406566d24a6fa15d6
AngelJostar <angelrojas@ciencias.unam.mx>
2021-02-19 14:31:56 -0600

    commit menu phone

--- includes/templates/top-header.php
+++ includes/templates/top-header.php
@@ -1,27 +1,27 @@
 <div id="top-header" class="top-header">
     <div class="top-header-inter">
+        <i class="fas fa-phone-volume"></i>
         <a href="tel:+5555238959">
             <span>
                         &nbsp; <span class="color-verde">Tel. CDMX:</span><br> 
                         <span>
-                        <i class="fas fa-phone"></i>
                         (55)5687-6118
                         </span>
             </span>
         </a>
+        <div class="linea-top-menu"></div>
         <a href="tel:+5555238959">
             <span>  &nbsp; <span class="color-verde">Tel. GDL: </span> <br>
                         <span>
-                        <i class="fas fa-phone"></i>
                         (33)3615-2835
                         </span>
             </span>
         </a>
+        <div class="linea-top-menu"></div>
         <a href="tel:+5555238959">
             <span>  
                         &nbsp; <span class="color-verde">Tel. MTY:</span> <br>
                         <span>
-                        <i class="fas fa-phone"></i>
                          (81)11592265
                         </span>
             </span>
