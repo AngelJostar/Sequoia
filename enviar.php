897798926f15f748187e8e3ef1d9ae1e64e4cacb
AngelJostar <angelrojas@ciencias.unam.mx>
2021-03-03 22:20:53 -0600

    commit banner prueba

--- enviar.php
+++ enviar.php
@@ -5,10 +5,11 @@
     $correo = $_POST["correo"];
     $telefono = $_POST["telefono"];
     $asunto = $_POST["asunto"];
+    $centro = $_POST["centro"];
     $mensaje = $_POST["mensaje"]; 
     
 
-    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nAsunto: " . $asunto . "\nMensaje: " . $mensaje;
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nAsunto: " . $asunto ."\nCentro". $centro . "\nMensaje: " . $mensaje;
     mail($destino, "Contacto: ", $contenido);
     header("Location:gracias.php");
 ?>
