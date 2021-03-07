899a8f5cd98814bc060acb2786bb3ba65013bcf1
AngelJostar <angelrojas@ciencias.unam.mx>
2021-03-07 00:39:27 -0600

    commit cambios grid

--- generalidades-del-crecimiento-infantil/etapas_del_desarollo_de_tenner.php
+++ generalidades-del-crecimiento-infantil/etapas_del_desarollo_de_tenner.php
@@ -25,30 +25,241 @@
 
         <div class="contendor-crecimiento">
 
+            <div class="cuadros-crecimiento">
+                <div class="titulos-grid-crecimiento-infantil">
+                    <div class="flexbox padre">
+                        <div class="barra-titulo"></div>
+                    </div>
+                    <h1><span class="font-bold">ETAPAS DEL DESARROLLO <br> DE TANNER</span></h1>
+                    <div class="flexbox padre">
+                        <div class="barra-titulo"></div>
+                    </div>
+                </div>
+            </div>
+
             <div class="caja-imagen-hojas">
-                <img src="/img/banners/Banner-doctor-doctora.webp" alt="">
+                <img src="../img/banners/Banner-doctor-doctora-etapas-tanner.webp" alt="">
                 <div class="texto-imagen-izquierda">
-                    <h2>El crecimiento y desarrollo físico es un proceso continuo y complejo, determinado por factores genéticos y modulado por factores ambientales, de tal modo que la máxima expresión de la potencialidad genética dependerá básicamente del estado de salud y nutrición del individuo. Una vez iniciado el desarrollo puberal, el proceso avanza en forma progresiva, completándose en un lapso promedio de 3 a 4 años. En las niñas la pubertad normalmente se completa a los 16 años y en los varones a los 18 años aproximadamente.</h2>
+                    <h2>El crecimiento y desarrollo físico es un proceso continuo y complejo, determinado por factores genéticos y modulado por factores ambientales, de tal modo que la máxima expresión de la potencialidad genética dependerá básicamente del
+                        estado de salud y nutrición del individuo. Una vez iniciado el desarrollo puberal, el proceso avanza en forma progresiva, completándose en un lapso promedio de 3 a 4 años. En las niñas la pubertad normalmente se completa a los
+                        16 años y en los varones a los 18 años aproximadamente.</h2>
                 </div>
             </div>
             <div class="justificar-texto-90">
-                <p>Para evaluar la progresión de los patrones normales de pubertad, los autores Tanner y Marshall describieron los “estadios de Tanner”, que es una clasificación que evalúa el grado de desarrollo puberal para ambos sexos, clasificándose en 5 estadios o grados lo que nos permite tener un lenguaje universal común siendo ampliamente aceptada en la práctica clínica. Es así como en las niñas se considera el desarrollo mamario y del vello pubiano, en los niños se describe el desarrollo de los genitales externos, principalmente el grado de volumen testicular, así como también se valora el vello pubiano.</p>
+                <p>
+                    De tal modo que la máxima expresión de la potencialidad genética dependerá básicamente del estado de salud y nutrición
+                    del individuo. Una vez iniciando el desarrollo puberal, el proceso avanza en forma progresiva, completándose en un lapso promedio
+                    de 3 a 4 años. En las niñas la pubertad normalmente se completa a los 16 años y en los varones a los 18 años aproximadamente.
+                </p>
+                <p>Para evaluar la progresión de los patrones normales de pubertad, los autores Tanner y Marshall describieron los “estadios de Tanner”, que es una clasificación que evalúa el grado de desarrollo puberal para ambos sexos, clasificándose en
+                    5 estadios o grados lo que nos permite tener un lenguaje universal común siendo ampliamente aceptada en la práctica clínica. Es así como en las niñas se considera el desarrollo mamario y del vello pubiano, en los niños se describe
+                    el desarrollo de los genitales externos, principalmente el grado de volumen testicular, así como también se valora el vello pubiano.</p>
             </div>
             <div class="justificar-texto-90">
                 <p>Estos estadios se enumeran en números romanos del I al V, siendo I el estado previo a la pubertad o sin evidencia de desarrollo puberal, mientras que el estadio V corresponde a las características obtenidas al alcanzar el desarrollo completo.</p>
             </div>
-            <div>
-            <p>Tabla I</p>
-            </div>
             <div class="justificar-texto-90">
-                <p>En general la pubertad progresa a razón de un estadio de Tanner por año. Esto es muy importante para evaluar si el proceso está ocurriendo normalmente o no.</p>    
+                <p>En general la pubertad progresa a razón de un estadio de Tanner por año. Esto es muy importante para evaluar si el proceso está ocurriendo normalmente o no.</p>
+            </div>
+            <div class="cuadros-crecimiento-2 efecto3-2 mt-1">
+                <div class="titulos-principal">
+                    <h2><span class="font-bold">TABLA I</span> <br> GRADOS DE TANNER DEL DESARROLLO <br> MAMARIO Y VELLO PUBIANO EN LAS NIÑAS</h2>
+                </div>
             </div>
             <div>
-            <p>Tabla II</p>
+                <div class="etapas-tenner-grid mt-1">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO I</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Ausencia de caracteres sexuales secundarios. No hay tejido mamario palpable, sólo el pezón protuye, la areola no está pigmentada.</li>
+                                    <li>No hay vello pubiano.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO II</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Etapa de botón mamario. Se palpa tejido mamario bajo la areola sin sobrepasar ésta.</li>
+                                    <li>Areola y pezón protuyen juntos, con aumento de diámetro areolar.</li>
+                                    <li>Es la etapa del botón mamario.</li>
+                                    <li>Vello pubiano incipiente a nivel de labios mayores.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO III</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Se caracteriza por crecimiento de la mama y aureola con pigmentación de ésta; el pezón ha aumentado de tamaño; la aureola y la mama tienen un solo contorno</li>
+                                    <li>Vello pubiano más abundante, alcanza la región púbica.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO VI</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Existe mayor aumento del volumen mamario con areola más pigmentada y solevantada, por lo que se observan 3 contornos (pezón, areola y mama). Vello pubiano en zona pubiana.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO V</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Vello pubiano más abundante</li>
+                                    <li>La mama es de tipo adulto, en el cual sólo el pezón protruye y la areola tiene el mismo contorno de la mama.</li>
+                                    <li>Alcanza cara interna inguinal</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="cuadros-crecimiento-2 efecto3-2 ">
+                    <div class="titulos-principal">
+                        <h2><span class="font-bold">TABLA II</span> <br> GRADOS DE TANNER DEL DESARROLLO <br> PUBERAL EN EL VARÓN</h2>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid mt-1">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO I</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Ausencia de caracteres sexuales secundarios.</li>
+                                    <li>Los testículos, escroto y pene tienen caracteristicas infantiles.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO II</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>El pene no se modifica, mientras el escroto y los testículos aumenten ligeramente de tamaño; la piel del escroto se oscurece y se modifica su estructura, haciéndose más laxa.</li>
+                                    <li>Volumen testicular de 4 cm3. Vello pubiano incipiente en la base del pene.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO III</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Se caracteriza por testículos y escroto más desarrollados. Vello pubiano alcanza el pubis.</li>
+                                    <li>Volumen testicular de 10 a 15 cm3.</li>
+                                    <li>El pene aumenta en grosor.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO IV</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Hay mayor crecimiento peneano, con aumento de su diámetro y desarrollo del glande.</li>
+                                    <li>Volumen testicular de 15 a 20 cm3. Vello pubiano con disposición de triángulo invertido.</li>
+                                    <li>Escroto está más desarrollado y pigmentado. Hay vello axilar, cambio de la tonalidad de la voz. Acné.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="linea-etapas"></div>
+                <div class="etapas-tenner-grid">
+                    <div class="etapas-grado">
+                        <div class="etapas-grado-bg">
+                            <p class="etapas-grado-hijo">GRADO V</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="contenedor-85porciento">
+                            <div class="justificar-texto-90-listas">
+                                <ul>
+                                    <li>Los genitales tienen forma y tamaño semejantes a los del adulto, con volumen testicular de 25cm3</li>
+                                    <li>Vello pubiano adquiere forma romboidal típica del adulto y extensión hacia cara medial del muslo.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="tamaño-img">
                 <img src="../img/banners/Banner-Estiron-Puberal.webp" alt="contacto crecimiento">
-            </div>           
+            </div>
         </div>
 
         <?php include_once '../includes/templates/footer.php';?>
