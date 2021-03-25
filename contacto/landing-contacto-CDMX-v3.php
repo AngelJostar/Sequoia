7a2c322416a893925080a362c22128368c714819
AngelJostar <angelrojas@ciencias.unam.mx>
2021-03-24 18:40:32 -0600

    commit cambios

--- contacto/landing-contacto-CDMX-v3.php
+++ contacto/landing-contacto-CDMX-v3.php
@@ -82,6 +82,76 @@
                     <p>Consulta a un especialista en talla baja de Centro Sequoia"</p>
                 </div>
             </div>
+            <section class="contenedor-info-contacto">
+
+                <div class="contacto-box ">
+
+                    <div>
+                        <div class="tercer-p">
+                            <h2 class="font-ciudades">CDMX<br>
+                                <a href="tel:+555687-6118">
+                                    <span>(55) 5687-6118</span>
+                                </a>
+                            </h2>
+                            <p><span class="font-bold">DIRECCIÓN:</span> San Francisco #516 Colonia Del Valle, </p>
+                            <p>Del. Benito Juárez. C.P. 03100, Méx, DF.</p>
+                            <p><span class="font-bold">EMAIL:</span> contacto@centrosequoia.com.mx </p>
+                            <p><span class="font-bold">HORARIO DE ATENCIÓN:</span></p> 
+                            <p>De Lunes a Viernes de 10:00 a 20:00 hrs.</p>
+                            <p>Sábados de 10:00 a 14:00 hrs.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="contenedor-90porciento">
+                    <a href="https://www.google.com/maps/place/Centro+Sequoia+CDMX-+Crecimiento+Infantil/@19.3916871,-99.1709422,15z/data=!4m5!3m4!1s0x0:0x1a23f6e38b618cce!8m2!3d19.3916871!4d-99.1709422"><img src="../img/Mapas/MAPA-CDMX-1015x542.webp" alt="Mapa-CDMX"></a>
+                </div>
+                <div class="contacto-grid">
+                    <div class="centrar-texto texto-boton-contacto pb-2 margin-top-20">
+                        <a href="https://www.google.com/maps/place/Centro+Sequoia+CDMX-+Crecimiento+Infantil/@19.3916871,-99.1709422,15z/data=!4m5!3m4!1s0x0:0x1a23f6e38b618cce!8m2!3d19.3916871!4d-99.1709422"><button class="boton-e boton-contacto">ABRIR UBICACIÓN <br> Google Maps</button></a>
+                    </div>
+                    <div class="centrar-texto texto-boton-contacto pb-2 margin-top-20">
+                        <a href="https://www.waze.com/live-map/directions?to=ll.19.391663%2C-99.170945"><button class="boton-e boton-contacto">ABRIR UBICACIÓN <br> Waze</button></a>
+                    </div>
+                </div>
+
+                <div class="linea-blanca"></div>
+                <div class="formulario">
+                    <form action="enviar " method="post">
+                    <div class="titulos-principal ">
+                        <h2>FORMULARIO DE CONTACTO<br><span class="letra-cursiva">Juntos ayudamos a tu hij@</span></h2>
+                    </div>
+                    <div class = "t-izquierda tercer-p">
+                        <p>NOMBRE *</p>
+                        <input type="text" name="nombre" placeholder="Nombre" required>
+                    </div>
+                    <div class = "t-izquierda tercer-p">
+                        <p>CORREO ELECTRONICO    *</p>
+                        <input type="email" name="correo" placeholder="Correo" required>
+                    </div>
+                    <div class = "t-izquierda tercer-p">
+                        <p>TELÉFONO</p>
+                        <input type="tel" name="telefono" placeholder="Teléfono" required>
+                    </div>
+                    <div class = "t-izquierda tercer-p">
+                        <p>ELIGE UN CENTRO *</p>
+                        <select name="centro" required>
+                        <option value="CDMX">CDMX</option> 
+                        <option value="GUADALAJARA">GUADALAJARA</option> 
+                        <option value="MOTERREY">MONTERREY</option>
+                        </select>
+                    </div>
+                    <div class = "t-izquierda tercer-p">
+                        <p>MENSAJE</p>
+                        <textarea name="mensaje" placeholder="Escriba aqui su mensaje" required></textarea>
+                    </div>
+                    <p class="justificar-texto-90 centrar-texto ph-1 input-color">Envianos tus dudas y conentarios</p>
+                        <div class="centrar-texto ph-1">
+                            <input type="submit" value="ENVIAR" id="boton">
+                        </div>
+                    </form>
+                </div>
+            </section>
             <div class="tamaño-img ocultar-media">
             <a href="/contacto"><img src="../img/banners/crecer-estatura-nino-crecimiento-infantil-2.webp" alt="causas crecimiento hijo"></a>
                 </div>
@@ -94,6 +164,7 @@
             <div class="centrar-texto pb-2">
                 <a href="causas-de-la-baja-estatura/baja-estatura-idiopatica"><button class="boton-e boton-contacto-landing">OTRAS UBICACIONES</button></a>
             </div>
+            
 
 
         </div>
